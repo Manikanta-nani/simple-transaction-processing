c3c1f5ba3ba43ecccae12d694c31a70ef709043a
manikanta <manikanta.kondiparthi@groupo.com>
2022-12-08 22:03:28 +0530

    updated

--- Customer.API/Controllers/CustomerController.cs
+++ Customer.API/Controllers/CustomerController.cs
@@ -11,10 +11,7 @@ namespace Customer.API.Controllers
     [Route("api/v1/[controller]")]
     public class CustomerController : ControllerBase
     {
-        private static readonly string[] Cutsomers = new[]
-        {
-        "Custom1", "Custom2", "Custom3", "Custom4", "Custom6"
-    };
+       
 
         private readonly ILogger<CustomerController> _logger;
         private IConnectionMultiplexer _connectionMultiplexer;
@@ -30,15 +27,45 @@ namespace Customer.API.Controllers
         {
             // var redisConnection= RedisService.TestConnection();
 
-
             IDatabase db = _connectionMultiplexer.GetDatabase();
+            
             string key = "customer:" + email.ToLower();
             string[] parms = { "." };
-            RedisResult result = await db.JsonGetAsync(key, parms);
+            RedisResult result = await db.JsonGetAsync(key, parms);            
             if (result.IsNull) { return null; };
             string profile = (string)result;
             return Ok(profile);
         }
+
+        [HttpPut(Name = "UpdateCustomer")]        
+        public async Task<ActionResult> UpdateCustomer(UpdateModel updateModel)
+        {
+           
+            IDatabase db = _connectionMultiplexer.GetDatabase();
+            string key = "customer:" + updateModel.Key.ToLower();
+            string[] parms = { "." };
+
+            try
+            {
+                if (updateModel.UpdateEntries.Any())
+                {
+                    foreach (var item in updateModel.UpdateEntries)
+                    {
+                        await db.JsonSetAsync(key, JsonConvert.SerializeObject(item.Value), item.Path);
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return Ok("Success");
+
+        }
+
 
         [HttpPost(Name = "SaveCustomer")]
         public async Task<ActionResult> SaveCustomer(CustomerModel customer)
@@ -46,22 +73,23 @@ namespace Customer.API.Controllers
             // var redisConnection= RedisService.TestConnection();
             IDatabase db = _connectionMultiplexer.GetDatabase();
             string key = "customer:" + customer.Email.ToLower();
+
             string json = JsonConvert.SerializeObject(customer);
             try
             {
                 OperationResult result = await db.JsonSetAsync(key, json);
                 if (result.IsSuccess) { return Ok("Save customer Profile Succeeded"); }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return BadRequest(ex.Message);
-            }         
+            }
 
-            
+
 
             return BadRequest("Save customer Profile  Failed");
-            
-        }
+
+        }       
 
     }
 }
---
+++ Customer.API/Models/UpdateModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Customer.API.Models
+{
+    public class UpdateModel
+    {       
+
+        public string Key { get; set; }
+
+        public UpdateEntry[] UpdateEntries { get; set; }
+    }
+
+    public class UpdateEntry
+    {
+        public string Value { get; set; }
+
+        public string Path { get; set; }
+    }
+}
